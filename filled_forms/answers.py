19674d88faff3cac7ae8f804de947a8406ea6dc6
KostylevEugene <kostylev_evgenij@list.ru>
2021-10-08 19:09:27 +0500

    I began post method in polls

---
+++ filled_forms/answers.py
@@ -0,0 +1,7 @@
+{
+    "Answers": {
+                    "What motherboard should I buy?": "a",
+                    "How much RAM should I buy?": "b",
+                    "How big monitor I need?": "c"
+    }
+}
